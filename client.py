f1c4600908723673121e09ab2ada23186f33a7cc
Mehthab <114091862+Meht4b@users.noreply.github.com>
2023-11-12 18:45:15 +0530

    ,

--- client.py
+++ client.py
@@ -1,7 +1,24 @@
+import pickle
 import socket
+import threading
+
+def receive_msg(socket):
+    while True:
+        response = pickle.loads(socket.recv(100))
+        print(f"{response[0]}:'{response[1]}'")
+
+def send_msg(socket):
+    while True:
+        user_input = input()
+        socket.send(pickle.dumps(user_input))
 
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect(('localhost',8080))
 
-print('kalsk')
-client_socket.close()
+name = print('enter name')
+send_msg(client_socket)
+
+thread_1 = threading.Thread(target=receive_msg,args=(client_socket))
+thread_2 = threading.Thread(target=send_msg,args=(client_socket))
+thread_1.start()
+thread_2.start()
--- server.py
+++ server.py
@@ -3,14 +3,25 @@ import socket
 import select
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 server_socket.bind(('localhost',8080))
 
 server_socket.listen()
 
+print('listening')
+
 socket_list = [server_socket]
+client_list = {}
+
+def broadcast(list,msg):
+    print('broadcast')
+    for socket in list:
+        socket.send(pickle.dumps(msg))
 
 def handle(conn):
-    pass
+    print('recvd')
+    broadcast(client_list.keys(),pickle.loads(conn.recv(100)))
+    print('broadcasted')
 
 while True:
 
@@ -19,7 +30,11 @@ while True:
     for unread_socket in read_socket:
         if unread_socket == server_socket:
             conn,addr = unread_socket.accept()
-            print(conn,'connected')
+            print(addr)
+            client_list[conn]=pickle.loads(conn.recv(100))
+            broadcast(client_list.keys(),f'{client_list[conn]} joined')
+
         else:
+            print('laksdjlk')
             handle(unread_socket)
 
